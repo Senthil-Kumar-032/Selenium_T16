32d6daced8e5d1d8c082619722ab2df23628d27e
Senthil Kumar S <senthilskumar032@gmail.com>
2026-04-06 07:54:54 +0530

    Commit

--- conftest.py
+++ conftest.py
@@ -3,8 +3,9 @@ from selenium import webdriver
 
 @pytest.fixture
 def setup():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    driver = webdriver.Chrome()        # Launch Chrome browser
+    driver.maximize_window()           # Maximize window
+    # Open target URL
     driver.get("https://www.theworldcounts.com/challenges/planet-earth/state-of-the-planet/world-population-clock-live")
-    yield driver
-    driver.quit()
+    yield driver                      # Provide driver to test
+    driver.quit()                     # Close browser after test
--- pages/population_page.py
+++ pages/population_page.py
@@ -7,18 +7,18 @@ class PopulationPage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 20)
+        self.wait = WebDriverWait(driver, 20)   # Wait up to 20 seconds
 
-    # ✅ Correct cookie XPath (your element)
+    # Correct cookie XPath (your element)
     cookie_button_xpath = "//a[contains(@class,'cc-dismiss') and contains(text(),'Got it')]"
 
-    # ✅ Population XPath
+    # Population XPath
     population_xpath = "(//span[contains(@class,'counter-number')])[1]"
 
     def accept_cookies(self):
-        """
-        Handle cookie popup
-        """
+
+        #   Handle cookie popup
+
         try:
             button = self.wait.until(
                 EC.element_to_be_clickable((By.XPATH, self.cookie_button_xpath))
@@ -31,7 +31,7 @@ class PopulationPage:
         try:
             element = self.wait.until(
                 EC.presence_of_element_located(
-                    (By.XPATH, "//div[contains(@class,'counter')]")
+                    (By.XPATH, "//div[contains(@class,'counter')]")     # Wait for counter section
                 )
             )
             text = element.text
@@ -39,7 +39,7 @@ class PopulationPage:
             # extract only numbers
             import re
             match = re.search(r'[\d,]{7,}', text)
-            return match.group() if match else "Not Found"
+            return match.group() if match else "Not Found"      # Return result
 
         except:
-            return "Not Found"
+            return "Not Found"                                  # If error
